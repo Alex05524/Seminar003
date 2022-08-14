9a4d667ac339a305c1531803f3e9fb35f4c96718
Alexey <pwz-and_10@mail.ru>
2022-08-14 21:27:52 +0600

    Fix Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3 - 1, 8, 27
+// 5 - 1, 8, 27, 64, 125
+
+Console.Write("Число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void table(int[] a)
+{
+  int count = 0;
+  int Length = a.Length;
+
+  while(count < Length)
+  {
+    a[count] = Convert.ToInt32(Math.Pow(count, 3));
+    count++;
+  }
+}
+
+void PrintArray(int[] collections)
+{
+  int count = collections.Length;
+  int i = 1;
+  while(i < count)
+  {
+    Console.Write(collections[i] + " ");
+    i++;
+  }
+}
+
+int[] array = new int[a + 1];
+table(array);
+PrintArray(array);
