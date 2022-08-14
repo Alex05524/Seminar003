18a3a7a6538f38895fe5ea011baa1b16a3a74bae
Alexey <pwz-and_10@mail.ru>
2022-08-14 17:09:29 +0600

    Create Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string a = Console.ReadLine();
+void CheckingNumber(string a)
+{
+if(a[0]== a[4] || a[1]== a[3])
+{
+    Console.WriteLine($"{a} - Да.");
+}
+else
+{
+    Console.WriteLine($"{a} - Нет.");
+}
+}
+if(a!.Length == 5)
+{
+    CheckingNumber(a);
+}
